570cd753d56a03e6dd40d73b9c84e347ce4fc71a
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:56:04 -0800

    Initial commit

---
+++ src/SumOfOddIntegers.java
@@ -0,0 +1,14 @@
+public class SumOfOddIntegers {
+    public static void main(String[] args) {
+
+        //this program is to print the sum of 3 odd integers between 0 and 5
+        int d, e, f;
+        d = 1;
+        e = 3;
+        f = 5;
+        int sum = d + e + f;
+        System.out.println(sum);
+    }
+}
+
+
